3e512034039a2101e58b174bab833782d715c722
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-03 15:35:24 +0700

    add global styles and PostCSS configuration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome to Firebits!</h1>
-            <p>Your one-stop solution for all your fire-related needs.</p>
+        <div className="home">
+            <h1 >Welcome to Firebits!</h1>
+            <p className="paragraphL">Your one-stop solution for all your fire-related needs.</p>
         </div>
     );
 }
