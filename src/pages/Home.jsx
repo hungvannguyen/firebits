fe36aa390e79a7c181e72395fa997a7cb3d88ef3
hungvannguyen <nguyenvanhung08012002@gmail.com>
2025-04-03 11:14:03 +0700

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Firebits!</h1>
+            <p>Your one-stop solution for all your fire-related needs.</p>
+        </div>
+    );
+}
